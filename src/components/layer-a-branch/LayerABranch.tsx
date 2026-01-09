7175ccda616fc7e74772e875cfc5d421ff2aab8c
Thomas Schlender <thebuoyant@gmail.com>
2026-01-09 19:34:50 +0100

    RH-22 Improve horizontal navigation for layer b

--- src/components/layer-a-branch/LayerABranch.tsx
+++ src/components/layer-a-branch/LayerABranch.tsx
@@ -5,34 +5,52 @@ import NavBadge from "../nav-badge/NavBadge";
 import "./LayerABranch.css";
 
 export default function LayerABranch() {
+  // layout store
   const cardWidth = useLayoutStore((s) => s.cardWidth);
   const cardSpace = useLayoutStore((s) => s.cardSpace);
 
   const branchLineItemWidth = 2 * cardSpace + cardWidth;
   const branchLineItemColor = APP_CONFIG.layout.branch.lineColor;
 
+  // card layer store
   const cardLayer_B_FirstItemIndexNumber = useCardLayerStore(
     (s) => s.cardLayer_B_FirstItemIndexNumber
   );
   const setCardLayer_B_FirstItemIndexNumber = useCardLayerStore(
     (s) => s.setCardLayer_B_FirstItemIndexNumber
   );
+  const cardLayer_B_Data = useCardLayerStore((s) => s.cardLayer_B_Data);
+
+  // internal calculations
+  const numberOfLayerBItems = cardLayer_B_Data.length;
 
   const handleClickLeft = () => {
-    console.log("left", cardLayer_B_FirstItemIndexNumber);
     setCardLayer_B_FirstItemIndexNumber(cardLayer_B_FirstItemIndexNumber - 1);
   };
 
   const handleClickRight = () => {
-    console.log("right", cardLayer_B_FirstItemIndexNumber);
     setCardLayer_B_FirstItemIndexNumber(cardLayer_B_FirstItemIndexNumber + 1);
   };
 
+  console.log("numberOfLayerBItems", numberOfLayerBItems);
+  console.log(
+    "cardLayer_B_FirstItemIndexNumber",
+    cardLayer_B_FirstItemIndexNumber
+  );
+
   return (
     <div className="layer-a-branch">
-      <div className="nav-item-left">
-        <NavBadge isVisible direction="left" onClick={handleClickLeft} />
-      </div>
+      {cardLayer_B_FirstItemIndexNumber > 0 &&
+        numberOfLayerBItems > APP_CONFIG.default.maxNumberOfCardsPerLayer && (
+          <div className="nav-item-left">
+            <NavBadge
+              isVisible
+              direction="left"
+              onClick={handleClickLeft}
+              counter={cardLayer_B_FirstItemIndexNumber}
+            />
+          </div>
+        )}
       <div className="branch-line">
         <div
           className="branch-line-item-a"
@@ -77,9 +95,23 @@ export default function LayerABranch() {
           }}
         ></div>
       </div>
-      <div className="nav-item-right">
-        <NavBadge isVisible direction="right" onClick={handleClickRight} />
-      </div>
+      {numberOfLayerBItems -
+        cardLayer_B_FirstItemIndexNumber -
+        APP_CONFIG.default.maxNumberOfCardsPerLayer >
+        0 && (
+        <div className="nav-item-right">
+          <NavBadge
+            isVisible
+            direction="right"
+            onClick={handleClickRight}
+            counter={
+              numberOfLayerBItems -
+              cardLayer_B_FirstItemIndexNumber -
+              APP_CONFIG.default.maxNumberOfCardsPerLayer
+            }
+          />
+        </div>
+      )}
     </div>
   );
 }
