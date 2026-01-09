38b29c274ed2465142efb4f68319545e9a17199e
Thomas Schlender <thebuoyant@gmail.com>
2026-01-09 19:57:57 +0100

    RH-24 Improve header navigation

--- src/components/header-nav/HeaderNav.tsx
+++ src/components/header-nav/HeaderNav.tsx
@@ -1,13 +1,27 @@
+import { APP_CONFIG } from "../../app.config";
+import { useCardLayerStore } from "../../store/cardLayerStore";
 import GraphBadge from "../graph-badge/GraphBadge";
 import NavBadge from "../nav-badge/NavBadge";
 import "./HeaderNav.css";
 
 export default function HeaderNav() {
+  // card layer store
+  const cardLayer_A_Data = useCardLayerStore((s) => s.cardLayer_A_Data);
+  const cardLayer_A_FirstItemIndexNumber = useCardLayerStore(
+    (s) => s.cardLayer_A_FirstItemIndexNumber
+  );
+
+  // internal calculations
+  const numberOfLayerAItems = cardLayer_A_Data.length;
+
   return (
     <div className="header-nav">
-      <div className="nav-item-left">
-        <NavBadge isVisible direction="left" />
-      </div>
+      {cardLayer_A_FirstItemIndexNumber > 0 &&
+        numberOfLayerAItems > APP_CONFIG.default.maxNumberOfCardsPerLayer && (
+          <div className="nav-item-left">
+            <NavBadge isVisible direction="left" />
+          </div>
+        )}
       <div className="nav-item-center">
         <GraphBadge
           counter={0}
@@ -17,9 +31,14 @@ export default function HeaderNav() {
           positionIndex={0}
         />
       </div>
-      <div className="nav-item-right">
-        <NavBadge isVisible direction="right" />
-      </div>
+      {numberOfLayerAItems -
+        cardLayer_A_FirstItemIndexNumber -
+        APP_CONFIG.default.maxNumberOfCardsPerLayer >
+        0 && (
+        <div className="nav-item-right">
+          <NavBadge isVisible direction="right" />
+        </div>
+      )}
     </div>
   );
 }
