1ac6b61ac4063c977e100aab7d94cdc9ff29fd65
Thomas Schlender <thebuoyant@gmail.com>
2026-01-09 18:56:37 +0100

    RH-20 Improve layer a branch

--- src/components/layer-a-branch/LayerABranch.tsx
+++ src/components/layer-a-branch/LayerABranch.tsx
@@ -1,19 +1,63 @@
+import { APP_CONFIG } from "../../app.config";
+import { useLayoutStore } from "../../store/layoutStore";
 import NavBadge from "../nav-badge/NavBadge";
 import "./LayerABranch.css";
 
 export default function LayerABranch() {
+  const cardWidth = useLayoutStore((s) => s.cardWidth);
+  const cardSpace = useLayoutStore((s) => s.cardSpace);
+
+  const branchLineItemWidth = 2 * cardSpace + cardWidth;
+  const branchLineItemColor = APP_CONFIG.layout.branch.lineColor;
+
   return (
     <div className="layer-a-branch">
       <div className="nav-item-left">
         <NavBadge isVisible direction="left" />
       </div>
       <div className="branch-line">
-        <div className="branch-line-item-a"></div>
-        <div className="branch-line-item-b"></div>
-        <div className="branch-line-item-c"></div>
-        <div className="branch-line-item-d"></div>
-        <div className="branch-line-item-e"></div>
-        <div className="branch-line-item-f"></div>
+        <div
+          className="branch-line-item-a"
+          style={{
+            width: branchLineItemWidth,
+            backgroundColor: branchLineItemColor,
+          }}
+        ></div>
+        <div
+          className="branch-line-item-b"
+          style={{
+            width: branchLineItemWidth,
+            backgroundColor: branchLineItemColor,
+          }}
+        ></div>
+        <div
+          className="branch-line-item-c"
+          style={{
+            width: branchLineItemWidth,
+            backgroundColor: branchLineItemColor,
+          }}
+        ></div>
+        <div
+          className="branch-line-item-d"
+          style={{
+            width: branchLineItemWidth,
+            backgroundColor: branchLineItemColor,
+          }}
+        ></div>
+        <div
+          className="branch-line-item-e"
+          style={{
+            width: branchLineItemWidth,
+            backgroundColor: branchLineItemColor,
+          }}
+        ></div>
+        <div
+          className="branch-line-item-f"
+          style={{
+            width: branchLineItemWidth,
+            backgroundColor: branchLineItemColor,
+          }}
+        ></div>
       </div>
       <div className="nav-item-right">
         <NavBadge isVisible direction="right" />
