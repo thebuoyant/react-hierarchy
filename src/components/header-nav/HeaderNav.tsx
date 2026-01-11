e6bc26933c180177db7007918f4689385ead9bba
Thomas Schlender <thebuoyant@gmail.com>
2026-01-11 16:41:59 +0100

    RH-28 Improve nav header badges

--- src/components/header-nav/HeaderNav.tsx
+++ src/components/header-nav/HeaderNav.tsx
@@ -13,6 +13,9 @@ export default function HeaderNav() {
   const cardLayer_Tmp_Data = useCardLayerStore((s) => s.cardLayer_Tmp_Data);
   const setCardLayer_A_Data = useCardLayerStore((s) => s.setCardLayer_A_Data);
   const cardLayer_B_Data = useCardLayerStore((s) => s.cardLayer_B_Data);
+  const cardLayer_B_FirstItemIndexNumber = useCardLayerStore(
+    (s) => s.cardLayer_B_FirstItemIndexNumber
+  );
   const setCardLayer_B_Data = useCardLayerStore((s) => s.setCardLayer_B_Data);
   const setCardLayer_C_Data = useCardLayerStore((s) => s.setCardLayer_C_Data);
   const setCardLayer_Tmp_Data = useCardLayerStore(
@@ -21,10 +24,10 @@ export default function HeaderNav() {
 
   // internal calculations
   const numberOfLayerAItems = cardLayer_A_Data.length;
+  const numberOfLayerBItems = cardLayer_B_Data.length;
 
   // actions
-  const handleBadgeClick = (item: any) => {
-    console.log("header badge", item);
+  const handleBadgeClick = () => {
     setCardLayer_A_Data(cardLayer_Tmp_Data);
     setCardLayer_B_Data(cardLayer_A_Data);
     setCardLayer_C_Data(cardLayer_B_Data);
@@ -36,13 +39,17 @@ export default function HeaderNav() {
       {cardLayer_A_FirstItemIndexNumber > 0 &&
         numberOfLayerAItems > APP_CONFIG.default.maxNumberOfCardsPerLayer && (
           <div className="nav-item-left">
-            <NavBadge isVisible direction="left" />
+            <NavBadge
+              isVisible
+              direction="left"
+              counter={cardLayer_A_FirstItemIndexNumber}
+            />
           </div>
         )}
       {numberOfLayerAItems >= APP_CONFIG.default.maxNumberOfCardsPerLayer && (
         <div className="nav-item-center">
           <GraphBadge
-            counter={0}
+            counter={cardLayer_Tmp_Data.length}
             isExpanded
             nodeId=""
             onClick={handleBadgeClick}
@@ -55,7 +62,15 @@ export default function HeaderNav() {
         APP_CONFIG.default.maxNumberOfCardsPerLayer >
         0 && (
         <div className="nav-item-right">
-          <NavBadge isVisible direction="right" />
+          <NavBadge
+            isVisible
+            direction="right"
+            counter={
+              numberOfLayerAItems -
+              cardLayer_A_FirstItemIndexNumber -
+              APP_CONFIG.default.maxNumberOfCardsPerLayer
+            }
+          />
         </div>
       )}
     </div>
